ae107ba6f0210e98798ae2d474fd425f44298c1d
Felix Tineo <felixtineo05@gmail.com>
2022-07-01 09:19:16 -0400

    initial commit

---
+++ src/handler.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.hello = async (event) => {
+  const { name } = JSON.parse(event.body);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Good day" + " " + name })
+  };
+};
