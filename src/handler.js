fc6242a36b05e59b665f5369f55bfc545d7a50ab
Felix Tineo <felixtineo05@gmail.com>
2022-07-01 09:43:45 -0400

    serverless.yml cors fix

--- src/handler.js
+++ src/handler.js
@@ -4,6 +4,6 @@ exports.hello = async (event) => {
   const { name } = JSON.parse(event.body);
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Good day" + " " + name })
+    body: JSON.stringify({ message: "Good afternoon" + " " + name })
   };
 };
